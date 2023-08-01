9045ca960ceb232da1e8c7fea039f39b4385e126
Geoffrey Lovelace <geoffrey4444@gmail.com>
2023-08-01 14:59:17 +0530

    Add pi dart serial

---
+++ CppCrashCourse/7_PiDart.cpp
@@ -0,0 +1,25 @@
+#include <cstddef>
+#include <random>
+#include <stdio.h>
+
+int main() {
+  std::random_device device;
+  std::mt19937_64 gen(device());
+  std::uniform_real_distribution dist{0.0, 1.0};
+
+  constexpr size_t throws = 1000;
+  size_t hits = 0;
+  for (size_t i = 0; i < throws; ++i) {
+    const double x = dist(gen);
+    const double y = dist(gen);
+    if (x * x + y * y < 1) {
+      hits += 1;
+    }
+  }
+
+  const double pi =
+      4.0 * static_cast<double>(hits) / static_cast<double>(throws);
+  printf("Hits: %lu Throws: %lu Pi Estimate: %1.15f\n", hits, throws, pi);
+
+  return 0;
+}
